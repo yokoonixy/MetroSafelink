63bb92744dbee1f33f36e5f138276d8f572e3e76
yokoonixy <secondlast.blog@gmail.com>
2017-02-02 12:06:25 +0700

    Create pagebuttonnextprev.js

---
+++ pagebuttonnextprev.js
@@ -0,0 +1,3 @@
+//<![CDATA[
+eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(2($){d b=$(\'a.q-e\');d c=$(\'a.p-e\');$.g(b.6(\'5\'),2(a){b.4(\'<3>r</3> <1>\'+$(a).9(\'.0 7.0-f\').8()+\'</1>\')},"4");$.g(c.6(\'5\'),2(a){c.4(\'<3>s</3> <1>\'+$(a).9(\'.0 7.0-f\').8()+\'</1>\')},"4")})(t);$(m).n(2(){o($("#h,.h").6("5")!="i://j.k.l"){u.v.5="i://j.k.l"}});',32,32,'post|span|function|strong|html|href|attr|h1|text|find||||var|link|title|get|creditlink2|http|www|seocips|com|document|ready|if|older|newer|Next|Previous|jQuery|window|location'.split('|'),0,{}))
+//]]>
